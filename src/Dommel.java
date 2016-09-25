87417fb3b15716799f20edd073e7b49f600fe90d
Vincent <vwierenga@gmail.com>
2016-09-25 11:10:17 +0200

    Added necessary classes

--- src/Dommel.java
+++ src/Dommel.java
@@ -2,4 +2,8 @@
  * Created by Vincent on 9/25/2016.
  */
 public class Dommel {
+    public static void main(String [] args)
+	{
+        System.out.println("test");
+    }
 }
---
+++ src/User.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Vincent on 9/25/2016.
+ */
+public class User {
+}
